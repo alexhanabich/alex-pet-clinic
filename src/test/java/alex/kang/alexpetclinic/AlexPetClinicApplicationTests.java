02113ceeec0d461bc1d519f39e216c055b1fe98f
Alex Kang <alexhanabich@gmail.com>
2020-04-19 20:51:15 -0600

    Init project

---
+++ src/test/java/alex/kang/alexpetclinic/AlexPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.kang.alexpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
